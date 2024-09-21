9b16b5791e5ae454b011b69286d454073167595b
herulobarto <herulobarto@gmail.com>
2024-09-21 20:56:48 +0700

    Membuat Back End dengan Filament

---
+++ app/Filament/Resources/SectionResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SectionResource\Pages;
+use App\Filament\Resources\SectionResource\RelationManagers;
+use App\Models\Section;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Notifications\Collection;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Storage;
+
+use function Livewire\store;
+
+class SectionResource extends Resource
+{
+    protected static ?string $model = Section::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-bookmark-square';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->required()->image()->disk('public'),
+                Forms\Components\RichEditor::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Select::make('post_as')->options([
+                    'JUMBOTRON' => 'JUMBOTRON',
+                    'ABOUT' => 'ABOUT',
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\ImageColumn::make('thumbnail'),                Tables\Columns\TextColumn::make('post_as')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            // fungsi ini digunakan untuk menghapus gambar lama secara multiple dan menghapus gambar di storage public
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make()->after(
+                        function (Collection $records) {
+                            foreach ($records as $key => $value) {
+                                if($value->thumbnail){
+                                    Storage::disk('public')->delete($value->thumbnail);
+                                }
+                            }
+                        }
+                    ),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSections::route('/'),
+            'create' => Pages\CreateSection::route('/create'),
+            'edit' => Pages\EditSection::route('/{record}/edit'),
+        ];
+    }
+}
